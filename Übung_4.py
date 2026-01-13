bf7aed2889775671b5b886a4ac2e7f7430dcba6b
Lorena110 <m25091@hb.dhbw-stuttgart.de>
2026-01-13 14:29:30 +0000

    übungen

--- Übung_4.py
+++ Übung_4.py
@@ -1,2 +1,20 @@
 temperatur = float(input("CNC.Kühlmittel-Temperatur (°C): "))
-zu_kalt = tempeatur
+betriebsbereich = ""
+idealbereich = ""
+empfehlung = ""
+
+if temperatur < 15 :
+    betriebsbereich = "Zu kalt: unter 15°C"
+    idealbereich = "Nein"
+    empfehlung = " Maschine STOPPEN! Kühlmittel erwärmen"
+
+if 15 < temperatur < 20 :
+    betriebsbereich = "Suboptimal kühl"
+    idealbereich = "Nein"
+    empfehlung = " funktionsfähig, aber nicht ideal"
+
+
+#print(f"CNC-Kühlmittel-Temperatur (°C): {temperatur} ")
+print(f"Betriebsbereich:{betriebsbereich}")
+print(f"Idealbereich (22-26°C):{idealbereich}")
+print(f"Empfehlung:{empfehlung}")
